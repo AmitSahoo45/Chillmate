1612c72944cabd610e2b666bcbb541e93b2ef357
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-04-01 13:15:55 +0530

    Updated File

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,10 @@
 import '../styles/globals.css'
 import { Navbar, Footer } from '../components'
 import { ProviderContext } from '../constants/context/Context'
-import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <html lang="en" />
-      </Head>
       <ProviderContext>
         <Navbar />
         <Component {...pageProps} />
